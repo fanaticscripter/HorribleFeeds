ddd4f014cce8525adbb2c3d45ac1c3a2a8d0ceaf
fanaticscripter <fanaticscripter@gmail.com>
2018-12-30 21:21:26 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import datetime
+import re
+import time
+import urllib.parse
+
+import arrow
+import bs4
+import flask
+import requests
+
+from preset import KNOWN_SHOWS
+
+
+app = flask.Flask(__name__)
+
+
+class Episode:
+    def __init__(self, title, date, magnet_link):
+        self.title = title
+        self.date = date
+        self.magnet_link = magnet_link
+
+    @property
+    def show_title(self):
+        return re.sub(r" \d+$", "", self.title)
+
+    @property
+    def btih(self):
+        return re.search(r"urn:btih:(?P<btih>\w+)\b", self.magnet_link).group("btih")
+
+    @property
+    def abbreviated_magnet_link(self):
+        return f"magnet:?xt=urn:btih:{self.btih}"
+
+    @property
+    def updated(self):
+        return min(
+            # Since HorribleSubs getshows API doesn't have time
+            # granularity greater than the date, we use the last second
+            # of the date in PST (timezone used by HorribleSubs
+            # website), provided that it doesn't exceed the current
+            # time.
+            arrow.now(),
+            arrow.get(self.date, "US/Pacific").shift(days=1, seconds=-1),
+        )
+
+
+@app.template_filter("iso8601")
+def format_iso8601(datetime):
+    return datetime.to("utc").replace(microsecond=0).isoformat().replace("+00:00", "Z")
+
+
+@app.route("/<int:showid>/")
+def feed(showid):
+    show_url = None
+    for url, sid in KNOWN_SHOWS:
+        if sid == showid:
+            show_url = urllib.parse.urljoin("https://horriblesubs.info/", url)
+    episodes = get_episodes(showid)
+    if episodes:
+        show_title = episodes[0].show_title
+    else:
+        if show_url:
+            # No episodes for a known show. The scraper is probably
+            # broken.
+            flask.abort(500)
+        show_title = "Unknown"
+    resp = flask.make_response(
+        flask.render_template(
+            "feed.atom",
+            showid=showid,
+            show_url=show_url,
+            title=f"{show_title} - HorribleSubs",
+            updated=arrow.now(),
+            episodes=episodes,
+        )
+    )
+    resp.headers["Content-Type"] = "application/atom+xml; charset=utf-8"
+    return resp
+
+
+# Returns a list of (title, date, magnet_link).
+def get_episodes(showid):
+    timestamp = int(time.time() * 1000)
+    api_url = f"https://horriblesubs.info/api.php?method=getshows&type=show&showid={showid}&nextid=0&_={timestamp}"
+    soup = bs4.BeautifulSoup(requests.get(api_url, timeout=5).text, "html.parser")
+    episodes = []
+    for container in soup.select(".rls-info-container"):
+        label = container.select_one(".rls-label")
+        date = label.select_one(".rls-date").text.strip()
+        date = datetime.datetime.strptime(date, "%m/%d/%y").date()
+        for span in label.select("span"):
+            span.extract()
+        title = label.text.strip()
+        a_magnet = container.select_one(".rls-link.link-1080p .hs-magnet-link a")
+        if a_magnet:
+            episodes.append(Episode(title, date, a_magnet["href"]))
+    return episodes
+
+
+def main():
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.jinja_env.auto_reload = True
+    app.run(debug=True, threaded=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ preset.py
@@ -0,0 +1,73 @@
+# List of current season shows (fetched 12/30/2018).
+#
+# If any show on this list returns blank, the scraper is probably
+# broken, and we should be noisy about it.
+#
+# curl -s https://horriblesubs.info/current-season/ | \
+#     grep -Po '/shows/[^"]+' | \
+#     sed 's:$:/:' | \
+#     while read show_url; do \
+#         echo "('$show_url', $(curl -s https://horriblesubs.info$show_url | grep -Po '(?<=hs_showid = )\d+')),"
+#         sleep 1
+#     done
+KNOWN_SHOWS = [
+    ('/shows/ace-attorney-s2/', 1173),
+    ('/shows/akanesasu-shoujo/', 1155),
+    ('/shows/anima-yell/', 1181),
+    ('/shows/bakumatsu/', 1163),
+    ('/shows/banana-fish/', 1112),
+    ('/shows/beelzebub-jou-no-okinimesu-mama/', 1190),
+    ('/shows/black-clover/', 959),
+    ('/shows/bonobono/', 694),
+    ('/shows/boruto-naruto-next-generations/', 869),
+    ('/shows/cardfight-vanguard-2018/', 1101),
+    ('/shows/chuukan-kanriroku-tonegawa/', 1109),
+    ('/shows/conception/', 1186),
+    ('/shows/dakaretai-otoko-1-i-ni-odosarete-imasu/', 1168),
+    ('/shows/detective-conan/', 97),
+    ('/shows/double-decker-doug-and-kirill/', 1147),
+    ('/shows/fairy-tail-final-season/', 1179),
+    ('/shows/gaikotsu-shotenin-honda-san/', 1183),
+    ('/shows/gakuen-basara/', 1172),
+    ('/shows/gegege-no-kitarou-2018/', 1058),
+    ('/shows/goblin-slayer/', 1175),
+    ('/shows/golden-kamuy/', 1090),
+    ('/shows/gurazeni/', 1074),
+    ('/shows/himote-house/', 1182),
+    ('/shows/hinomaru-sumo/', 1166),
+    ('/shows/irozuku-sekai-no-ashita-kara/', 1171),
+    ('/shows/jingai-san-no-yome/', 1158),
+    ('/shows/jojos-bizarre-adventure-golden-wind/', 1169),
+    ('/shows/karakuri-circus/', 1188),
+    ('/shows/kaze-ga-tsuyoku-fuiteiru/', 1159),
+    ('/shows/ken-en-ken-aoki-kagayaki/', 1157),
+    ('/shows/kishuku-gakkou-no-juliet/', 1170),
+    ('/shows/kitsune-no-koe/', 1193),
+    ('/shows/merc-storia-mukiryoku-shounen-to-bin-no-naka-no-shoujo/', 1187),
+    ('/shows/one-piece/', 347),
+    ('/shows/ore-ga-suki-nano-wa-imouto-dakedo-imouto-ja-nai/', 1185),
+    ('/shows/radiant/', 1174),
+    ('/shows/release-the-spyce/', 1178),
+    ('/shows/rerided-tokigoe-no-derrida/', 1151),
+    ('/shows/saint-seiya-saintia-shou/', 1195),
+    ('/shows/seishun-buta-yarou-wa-bunny-girl-senpai-no-yume-wo-minai/', 1161),
+    ('/shows/senran-kagura-shinovi-master-tokyo-youma-hen/', 1191),
+    ('/shows/shounen-ashibe-go-go-goma-chan/', 667),
+    ('/shows/sora-to-umi-no-aida/', 1160),
+    ('/shows/souten-no-ken-re-genesis/', 1062),
+    ('/shows/ssss-gridman/', 1177),
+    ('/shows/sword-art-online-alicization/', 1176),
+    ('/shows/tensei-shitara-slime-datta-ken/', 1152),
+    ('/shows/the-idolmster-side-m-wake-atte-mini/', 1184),
+    ('/shows/thunderbolt-fantasy-s2/', 1154),
+    ('/shows/toaru-majutsu-no-index-iii/', 1189),
+    ('/shows/tokyo-ghoul-re/', 1068),
+    ('/shows/tonari-no-kyuuketsuki-san/', 1164),
+    ('/shows/tsurune/', 1192),
+    ('/shows/uchi-no-maid-ga-uzasugiru/', 1167),
+    ('/shows/uchuu-senkan-tiramisu-s2/', 1156),
+    ('/shows/ulysses-jeanne-darc-to-renkin-no-kishi/', 1180),
+    ('/shows/yagate-kimi-ni-naru/', 1165),
+    ('/shows/yu-gi-oh-vrains/', 901),
+    ('/shows/zombieland-saga/', 1162),
+]
